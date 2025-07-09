a17c01acebef829f15ae9f993b5969aa156074d8
Edvárd Laub <edvard-work@Edvards-second-Macbook.local>
2025-07-09 12:19:29 +0300

    Added updated files

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/edvard-work/Documents/Task_Manager/venv"
+set -gx VIRTUAL_ENV "/Users/edvard-work/task-manager/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
